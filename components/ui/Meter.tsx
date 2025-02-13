be3d21ce876f9c848cdda7c55f279182debe2bec
solarin-johnson <solarinjohnson@gmail.com>
2025-02-13 15:58:34 +0100

    add react-native-redash dependency; refactor Monitor component for improved performance and support for animated values

--- components/ui/Meter.tsx
+++ components/ui/Meter.tsx
@@ -1,11 +1,12 @@
 import { useThemeColor } from "@/hooks/useThemeColor";
-import React, { Fragment } from "react";
+import React, { Fragment, memo } from "react";
 import {
   View,
   StyleSheet,
   ViewStyle,
   useWindowDimensions,
   Platform,
+  Text,
 } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
@@ -20,11 +21,14 @@ import Animated, {
 } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Svg, { Circle, G, Line, Path } from "react-native-svg";
+import { ReText } from "react-native-redash";
+import Monitor, { MonitorCard } from "./Monitor";
 
 const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 const AnimatedLine = Animated.createAnimatedComponent(Line);
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 const AnimatedG = Animated.createAnimatedComponent(G);
+const AnimatedText = Animated.createAnimatedComponent(Text);
 
 const calculatePercentage = (value: number, maxValue: number) => {
   return Math.min(Math.max((value / maxValue) * 100, 0), 96) / 100;
@@ -37,12 +41,7 @@ interface MeterProps {
   updateValue?: any;
 }
 
-export const Meter: React.FC<MeterProps> = ({
-  value,
-  maxValue,
-  style,
-  updateValue,
-}) => {
+export const Meter: React.FC<MeterProps> = ({ value, maxValue, style }) => {
   const currentValue = useSharedValue(value);
   const percentage = useSharedValue(calculatePercentage(value, maxValue));
   const depth = useSharedValue(0);
@@ -50,10 +49,7 @@ export const Meter: React.FC<MeterProps> = ({
   const insets = useSafeAreaInsets();
   const { height } = useWindowDimensions();
   const meterHeight = height - 70 - insets.top - insets.bottom;
-
-  useDerivedValue(() => {
-    runOnJS(updateValue)(currentValue.value);
-  }, [currentValue]);
+  const currentValueString = useSharedValue(`${value}`);
 
   const panGesture = Gesture.Pan()
     .onBegin(() => {
@@ -61,27 +57,41 @@ export const Meter: React.FC<MeterProps> = ({
       pressed.value = true;
     })
     .onUpdate(({ y }) => {
-      const newValue = Math.min(
-        Math.max(currentValue.value - y / 10, 0),
-        maxValue
+      const newValue = Math.round(
+        Math.min(Math.max(currentValue.value - y / 10, 0), maxValue)
       );
+
       currentValue.value = newValue;
+      currentValueString.value = `${newValue}`;
 
       percentage.value =
         Math.min(Math.max((newValue / maxValue) * 100, 0), 96) / 100;
     })
     .onFinalize(() => {
-      depth.value = withSpring(0, { duration: 200 });
-      pressed.value = false;
+      if (pressed.value) {
+        depth.value = withSpring(0, { duration: 200 });
+        pressed.value = false;
+      }
     });
 
   return (
-    <View style={[styles.container, { height: meterHeight }, style]}>
-      <Scales height={meterHeight} percent={percentage} pressed={depth} />
-      <GestureDetector gesture={panGesture}>
-        <Handle pressed={pressed} percent={percentage} height={meterHeight} />
-      </GestureDetector>
-    </View>
+    <>
+      <View style={[styles.container, { height: meterHeight }, style]}>
+        <Scales height={meterHeight} percent={percentage} pressed={depth} />
+        <GestureDetector gesture={panGesture}>
+          <Handle pressed={pressed} percent={percentage} height={meterHeight} />
+        </GestureDetector>
+        {/* <ReText text={currentValueString} style={{ color: "red" }} /> */}
+      </View>
+      <Monitor>
+        <MonitorCard label="Humidity" value={45} unit="%" faded />
+        <MonitorCard
+          label="Temperature"
+          animatedValue={currentValueString}
+          unit="°C"
+        />
+      </Monitor>
+    </>
   );
 };
 
@@ -91,14 +101,41 @@ interface ScalesProps {
   percent?: any;
   pressed: any;
 }
-const Scales: React.FC<ScalesProps> = ({ percent, height, pressed }) => {
-  const totalLines = 200; // Ensure equal lines on all devices
+
+const Scales: React.FC<ScalesProps> = memo(({ percent, height, pressed }) => {
+  const totalLines = 120; // Ensure equal lines on all devices
   const gap = height / totalLines;
   const strokeColor = useThemeColor({}, "text");
   const lowColor = useThemeColor({}, "low");
   const highColor = useThemeColor({}, "high");
   const tapeWidth = 54;
-  const range = 15;
+  const range = 10;
+
+  const current = useDerivedValue(() => {
+    return totalLines - Math.floor(totalLines * percent.value);
+  });
+  const startRange = useDerivedValue(() => {
+    return current.value - range;
+  });
+  const endRange = useDerivedValue(() => {
+    return startRange.value + range;
+  });
+
+  const colorStops = useDerivedValue(() => {
+    return interpolateColor(percent.value, [0, 1], [highColor, lowColor]);
+  });
+
+  const calcFilled = useDerivedValue(() => {
+    return endRange.value - range + 5;
+  });
+
+  const normStartRange = useDerivedValue(() => {
+    return startRange.value - range / 4;
+  });
+
+  const normendRange = useDerivedValue(() => {
+    return endRange.value + range / 4;
+  });
 
   const renderLines = (isSecondSet: boolean) => {
     return [...Array(totalLines)].map((_, i) => {
@@ -108,32 +145,42 @@ const Scales: React.FC<ScalesProps> = ({ percent, height, pressed }) => {
         ? tapeWidth - tapeWidth / 4.2
         : tapeWidth - tapeWidth / 7;
 
+      const isInRange = useDerivedValue(() => {
+        return i >= normStartRange.value && i <= normendRange.value;
+      });
+
+      const filled = useDerivedValue(() => {
+        return percent.value <= 0 ? false : i > calcFilled.value;
+      });
+
+      // Derived value for curveFactor
+      const curveFactor = useDerivedValue(() => {
+        if (isInRange.value) {
+          const normalized =
+            (i - normStartRange.value) /
+            (endRange.value - startRange.value - range / 4);
+          return -(1 - Math.cos(normalized * Math.PI)) * pressed.value;
+        }
+        return 0;
+      });
+
+      const strokeColorAnimated = useDerivedValue(() => {
+        return isSecondSet
+          ? !filled.value
+            ? strokeColor
+            : colorStops.value
+          : strokeColor;
+      });
+      const opacity = useDerivedValue(() => (filled.value ? 1 : 0.3));
+
       const animatedProps = useAnimatedProps(() => {
         "worklet";
-        const current = totalLines - Math.floor(totalLines * percent.value);
-        const startRange = current - range;
-        const endRange = startRange + range;
-        let curveFactor = 0;
-        const filled = percent.value <= 0 ? 0 : i > endRange - range + 5;
-
-        if (startRange - range / 4 <= i && i < endRange + range / 4) {
-          const normalized =
-            (i - (startRange - range / 4)) /
-            (endRange - startRange - range / 4);
-          curveFactor = (1 - Math.cos(normalized * Math.PI)) * pressed.value;
-        }
 
         return {
-          transform: [{ translateX: -curveFactor }],
-          stroke: isSecondSet
-            ? !filled
-              ? strokeColor
-              : interpolateColor(percent.value, [0, 1], [highColor, lowColor])
-            : strokeColor,
-          strokeWidth: isSecondSet
-            ? withSpring(pressed.value ? 3.2 : 2.8, { duration: 200 })
-            : 1.5,
-          ...(isSecondSet && { opacity: filled ? 1 : 0.3 }),
+          transform: [{ translateX: curveFactor.value }],
+          stroke: strokeColorAnimated.value,
+          strokeWidth: isSecondSet ? 2.8 : 1.5,
+          ...(isSecondSet && { opacity: opacity.value }),
         };
       });
 
@@ -168,14 +215,14 @@ const Scales: React.FC<ScalesProps> = ({ percent, height, pressed }) => {
       </AnimatedSvg>
     </View>
   );
-};
+});
 
 const Handle: React.FC<{
   pressed: any;
   percent?: any;
   height: number;
   setValue?: Function;
-}> = ({ pressed, percent, height }) => {
+}> = memo(({ pressed, percent, height }) => {
   const strokeColor = useThemeColor({}, "text");
   const isWeb = Platform.OS === "web";
   const range = isWeb ? 13 : 15.5;
@@ -201,8 +248,9 @@ const Handle: React.FC<{
     const current = height - Math.floor(height * percent.value);
 
     return {
-      height: 20,
-      marginTop: current - range * 2,
+      height: 80,
+      marginTop: current - range * 2 - 22,
+      // backgroundColor: "red",
     };
   });
 
@@ -215,37 +263,39 @@ const Handle: React.FC<{
       })}
     >
       <AnimatedSvg
-        width={50}
+        width={80}
         fill="transparent"
         style={!isWeb && animatedStyle}
       >
-        <AnimatedCircle
-          cx={20}
-          cy={10}
-          fill={strokeColor}
-          stroke={strokeColor}
-          strokeWidth={1.5}
-          animatedProps={animatedProps}
-        />
+        <G y={30} x={4}>
+          <AnimatedCircle
+            cx={20}
+            cy={10}
+            fill={strokeColor}
+            stroke={strokeColor}
+            strokeWidth={1.5}
+            animatedProps={animatedProps}
+          />
 
-        <AnimatedG
-          scale={0.7}
-          animatedProps={handleAnimatedProps}
-          translateX={13}
-        >
-          <Path
-            d="M9.1583 1.31208C9.5518 0.698676 10.4482 0.698676 10.8417 1.31208L17.6724 11.9601C18.0993 12.6256 17.6214 13.5 16.8307 13.5H3.16932C2.37859 13.5 1.90067 12.6256 2.32762 11.9601L9.1583 1.31208Z"
-            fill={strokeColor}
-          />
-          <Path
-            d="M10.8417 27.488C10.4482 28.1014 9.5518 28.1014 9.1583 27.488L2.32762 16.84C1.90067 16.1744 2.37859 15.3 3.16932 15.3L16.8307 15.3C17.6214 15.3 18.0993 16.1744 17.6724 16.84L10.8417 27.488Z"
-            fill={strokeColor}
-          />
-        </AnimatedG>
+          <AnimatedG
+            scale={0.7}
+            animatedProps={handleAnimatedProps}
+            translateX={13}
+          >
+            <Path
+              d="M9.1583 1.31208C9.5518 0.698676 10.4482 0.698676 10.8417 1.31208L17.6724 11.9601C18.0993 12.6256 17.6214 13.5 16.8307 13.5H3.16932C2.37859 13.5 1.90067 12.6256 2.32762 11.9601L9.1583 1.31208Z"
+              fill={strokeColor}
+            />
+            <Path
+              d="M10.8417 27.488C10.4482 28.1014 9.5518 28.1014 9.1583 27.488L2.32762 16.84C1.90067 16.1744 2.37859 15.3 3.16932 15.3L16.8307 15.3C17.6214 15.3 18.0993 16.1744 17.6724 16.84L10.8417 27.488Z"
+              fill={strokeColor}
+            />
+          </AnimatedG>
+        </G>
       </AnimatedSvg>
     </Wrapper>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
--- components/ui/Monitor.tsx
+++ components/ui/Monitor.tsx
@@ -1,43 +1,49 @@
-import React from "react";
+import React, { memo } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { ThemedText } from "../ThemedText";
+import { ReText } from "react-native-redash";
+import { SharedValue } from "react-native-reanimated";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 interface MonitorCardProps {
   label: string;
-  value: string | number;
+  value?: string | number | SharedValue<string>;
+  animatedValue?: SharedValue<string>;
   unit: string;
   faded?: boolean;
 }
 
-export const MonitorCard: React.FC<MonitorCardProps> = ({
-  label,
-  value,
-  unit,
-  faded,
-}) => {
-  return (
-    <View style={styles.container}>
-      <ThemedText style={styles.label}>{label}</ThemedText>
-      <View style={[styles.valueContainer, faded && styles.faded]}>
-        <ThemedText style={styles.value}>{`${value}${unit}`}</ThemedText>
+export const MonitorCard: React.FC<MonitorCardProps> = memo(
+  ({ label, value, unit, faded, animatedValue }) => {
+    const color = useThemeColor({}, "text");
+    return (
+      <View style={styles.container}>
+        <ThemedText style={styles.label}>{label}</ThemedText>
+        <View style={[styles.valueContainer, faded && styles.faded]}>
+          {animatedValue ? (
+            <>
+              <ReText
+                style={[styles.value, { color, marginRight: -4 }]}
+                text={animatedValue}
+              />
+              <ThemedText style={styles.value}>{unit}</ThemedText>
+            </>
+          ) : (
+            <ThemedText style={styles.value}>{`${value}${unit}`}</ThemedText>
+          )}
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+);
 
 interface MonitorProps {
-  cards: MonitorCardProps[];
+  children?: React.ReactNode;
 }
 
-const Monitor: React.FC<MonitorProps> = ({ cards }) => {
-  return (
-    <View style={styles.monitorContainer}>
-      {cards.map((card, index) => (
-        <MonitorCard key={index} {...card} />
-      ))}
-    </View>
-  );
-};
+const Monitor: React.FC<MonitorProps> = memo(({ children }) => {
+  return <View style={styles.monitorContainer}>{children}</View>;
+});
 
 const styles = StyleSheet.create({
   monitorContainer: {
@@ -47,6 +53,7 @@ const styles = StyleSheet.create({
     gap: 6,
     paddingVertical: 42,
     paddingHorizontal: 8,
+    // backgroundColor: "red",
   },
   container: {
     padding: 16,
